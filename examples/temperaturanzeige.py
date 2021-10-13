8ddf0aaed87a641116fb765f6927e1bbbe7d8d3d
adb <info@adb.sh>
2021-10-13 19:24:33 +0200

    add temperaturanzeige.py

---
+++ examples/temperaturanzeige.py
@@ -0,0 +1,51 @@
+import board
+import digitalio
+import glob
+import time
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+RESET_PIN = digitalio.DigitalInOut(board.D4)
+
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=RESET_PIN)
+
+oled.fill(0)
+oled.show()
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 28)
+font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+
+path = "/sys/bus/w1/devices/"
+sensor_path = glob.glob(path + "28*")[0]
+sensor_data_path = sensor_path + "/w1_slave"
+
+def read_temperature():
+  file = open(sensor_data_path, "r")
+  rows = file.readlines()
+  file.close()
+  return rows
+
+def get_temperature_in_grad():
+  rows = read_temperature()
+  while rows[0].strip()[-3:] != 'YES':
+    time.sleep(0.2)
+    rows = temperatur_lesen()
+  equals_pos = rows[1].find('t=')
+  if equals_pos != -1:
+      temp_string = rows[1][equals_pos+2:]
+      temp_c = float(temp_string) / 1000.0
+      return temp_c
+
+
+while True:
+  image = Image.new("1", (oled.width, oled.height))
+  draw = ImageDraw.Draw(image)
+
+  draw.text((0, 0), "Temperatur:", font=font2, fill=255)
+  draw.text((0, 30), str(get_temperature_in_grad())+"°C", font=font, fill=255)
+
+  oled.image(image)
+  oled.show()
+
+  time.sleep(1)
