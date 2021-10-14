3547e58321c5dc3c66c5849932743e799098219c
adb-sh <git@adb.sh>
2021-10-14 13:56:19 +0200

    fix temperaturanzeige.py

--- examples/temperaturanzeige.py
+++ examples/temperaturanzeige.py
@@ -1,7 +1,4 @@
-import board
-import digitalio
-import glob
-import time
+import board, digitalio, glob, time
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
@@ -26,24 +23,23 @@ def read_temperature():
   file.close()
   return rows
 
-def get_temperature_in_grad():
+def get_temperature_in_degree():
   rows = read_temperature()
   while rows[0].strip()[-3:] != 'YES':
     time.sleep(0.2)
-    rows = temperatur_lesen()
+    rows = read_temperature()
   equals_pos = rows[1].find('t=')
   if equals_pos != -1:
       temp_string = rows[1][equals_pos+2:]
       temp_c = float(temp_string) / 1000.0
       return temp_c
 
-
 while True:
   image = Image.new("1", (oled.width, oled.height))
   draw = ImageDraw.Draw(image)
 
   draw.text((0, 0), "Temperatur:", font=font2, fill=255)
-  draw.text((0, 30), str(get_temperature_in_grad())+"°C", font=font, fill=255)
+  draw.text((0, 30), str(get_temperature_in_degree())+"°C", font=font, fill=255)
 
   oled.image(image)
   oled.show()
