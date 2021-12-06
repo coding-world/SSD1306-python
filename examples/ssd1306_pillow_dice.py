08186956b45613305abaf5f000a6580ec3345369
//ADB <38115351+adb-sh@users.noreply.github.com>
2021-12-06 14:42:47 +0100

    Create ssd1306_pillow_dice.py

---
+++ examples/ssd1306_pillow_dice.py
@@ -0,0 +1,53 @@
+import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
+from random import randint
+import RPi.GPIO as gpio
+import digitalio
+import time
+import board
+
+# define pins
+gpio.setmode(gpio.BCM)
+channel = 23
+gpio.setup(channel, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+RESET_PIN = digitalio.DigitalInOut(board.D4)
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=RESET_PIN)
+
+# clear display
+oled.fill(0)
+oled.show()
+
+# import font
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 64)
+
+# print ?
+imageBegin = Image.new("1", (oled.width, oled.height))
+ImageDraw.Draw(imageBegin).text((40, 0), "?", font=font, fill=255)
+oled.image(imageBegin)
+oled.show()
+
+# generate images
+numbers = []
+for i in range(1, 7):
+    image = Image.new("1", (oled.width, oled.height))
+    ImageDraw.Draw(image).text((40, 0), str(i), font=font, fill=255)
+    numbers.append(image)
+
+
+def show_random():
+    oled.image(numbers[randint(0, 5)])
+    oled.show()
+    time.sleep(0.01)
+
+
+while True:
+    if gpio.input(channel) == 0:
+        while True:
+            show_random()
+            if gpio.input(channel) == 1:
+                show_random()
+                time.sleep(0.5)
+                break
+        time.sleep(0.05)
