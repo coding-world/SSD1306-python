6da5f23be23c6cb1a3d77c31e721e178d76fae30
adb-sh <git@adb.sh>
2021-12-08 11:48:03 +0100

    simplify openweather

--- examples/ssd1306_pillow_openweather.py
+++ examples/ssd1306_pillow_openweather.py
@@ -5,11 +5,11 @@ import board
 import digitalio
 import requests
 import time
+import os.path
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 from io import BytesIO
 import cairosvg
-import os.path
 
 # set pins and init oled
 RESET_PIN = digitalio.DigitalInOut(board.D4)
@@ -22,8 +22,8 @@ oled.show()
 
 # load fonts
 font1 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 10)
-font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 12)
-font3 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+font3 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16)
 
 
 def get_icon(id, size=32):
@@ -33,7 +33,7 @@ def get_icon(id, size=32):
         cairosvg.svg2png(url=url, write_to=out)  # convert svg to png
         return Image.open(out).resize((size, size)).convert('1')  # return resized image
     else:
-        return Image.new("1", (32, 32))  # return empty image
+        return Image.new("1", (size, size))  # return empty image
 
 
 while True:
@@ -45,21 +45,21 @@ while True:
             '&lang=de'                                  # referred language
             '&lat=54.788'                               # location (latitude)
             '&lon=9.43701',                             # location (longitude)
-        timeout=5
+        timeout=10
     ).json()
 
     # display hourly data
     for step in [
-        {'name': 'Jetzt:', 'data': data['current']},
-        {'name': 'in einer Stunde:', 'data': data['hourly'][1]},
-        {'name': 'in zwei Stunden:', 'data': data['hourly'][2]},
-        {'name': 'in 3 Stunden:', 'data': data['hourly'][3]},
-        {'name': 'in 6 Stunden:', 'data': data['hourly'][6]},
+        {'title': 'Jetzt:', 'data': data['current']},
+        {'title': 'in einer Stunde:', 'data': data['hourly'][1]},
+        {'title': 'in zwei Stunden:', 'data': data['hourly'][2]},
+        {'title': 'in 3 Stunden:', 'data': data['hourly'][3]},
+        {'title': 'in 6 Stunden:', 'data': data['hourly'][6]},
     ]:
         image = Image.new("1", (oled.width, oled.height))
         draw = ImageDraw.Draw(image)
 
-        draw.text((0, 0), step['name'], font=font3, fill=255)
+        draw.text((0, 0), step['title'], font=font3, fill=255)
         draw.text((0, 16), step['data']['weather'][0]['description'], font=font2, fill=255)
         draw.text((48, 32), str(step['data']['temp'])+'°C', font=font3, fill=255)
         draw.text((48, 48), str(step['data']['humidity'])+'%', font=font3, fill=255)
@@ -67,28 +67,17 @@ while True:
 
         oled.image(image)
         oled.show()
-        time.sleep(2)
+        time.sleep(4)
 
-    # display daily data
-    for step in [
-        {'name': 'Morgen:', 'data': data['daily'][1]},
-        {'name': 'Übermorgen:', 'data': data['daily'][2]},
-        {'name': 'in 3 Tagen:', 'data': data['daily'][3]},
-    ]:
-        image = Image.new("1", (oled.width, oled.height))
-        draw = ImageDraw.Draw(image)
+    image = Image.new("1", (oled.width, oled.height))
+    draw = ImageDraw.Draw(image)
 
-        draw.text((0, 0), step['name'], font=font3, fill=255)
-        draw.text((0, 16), 'Morgens:', font=font2, fill=255)
-        draw.text((0, 32), 'Mittags:', font=font2, fill=255)
-        draw.text((0, 48), 'Abends:', font=font2, fill=255)
-        draw.text((60, 16), str(step['data']['temp']['morn'])+'°C', font=font2, fill=255)
-        draw.text((60, 32), str(step['data']['temp']['day'])+'°C', font=font2, fill=255)
-        draw.text((60, 48), str(step['data']['temp']['eve'])+'°C', font=font2, fill=255)
-        image.paste(get_icon(step['data']['weather'][0]['icon'], 16), (112, 16))
-        image.paste(get_icon(step['data']['weather'][0]['icon'], 16), (112, 32))
-        image.paste(get_icon(step['data']['weather'][0]['icon'], 16), (112, 48))
+    draw.text((0, 0), 'nächste Tage', font=font3, fill=255)
+    for i in range(1, 4):
+        draw.text((24, 16 * i), str(data['daily'][i]['temp']['day'])[:4] + '°C', font=font2, fill=255)
+        draw.text((76, 16 * i), str(data['daily'][i]['temp']['night'])[:4] + '°C', font=font2, fill=255)
+        image.paste(get_icon(data['daily'][i]['weather'][0]['icon'], 16), (0, 16 * i))
 
-        oled.image(image)
-        oled.show()
-        time.sleep(5)
+    oled.image(image)
+    oled.show()
+    time.sleep(8)
